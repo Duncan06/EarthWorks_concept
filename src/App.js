5b3d8102d0e6a45ac82a1d8893ca6415893815ed
Duncan06 <tylerdownie0@gmail.com>
2022-03-25 12:37:49 -0700

    First commit.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navigation from "./components/Navigation";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {Navigation()}
+      </header>
+    </div>
+  );
+}
+
+export default App;
