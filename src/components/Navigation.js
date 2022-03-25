c79ed7440a9e8254768d6cfd8ca7456149876e7d
Duncan06 <tylerdownie0@gmail.com>
2022-03-25 14:40:06 -0700

    Add css to nav.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,2 +1,16 @@
-import "./Navigation.module.css";
+import nav from "./Navigation.module.css";
 
+function Navigation() {
+  return (
+    <div>
+      <header className={nav.navMain}>
+        <div className={nav.navButton}>Home</div>
+        <div className={nav.navButton}>About us</div>
+        <div className={nav.navButton}>Pricing/Estimates</div>
+        <div className={nav.navButton}>Contact For Hire</div>
+      </header>
+    </div>
+  );
+}
+
+export default Navigation;
