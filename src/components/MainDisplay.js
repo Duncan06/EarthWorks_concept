2103a7c7384c9f62836dbef10d928303a96fca75
Duncan06 <tylerdownie0@gmail.com>
2022-03-26 12:22:37 -0700

    Add info to main container.

--- src/components/MainDisplay.js
+++ src/components/MainDisplay.js
@@ -7,7 +7,24 @@ function MainDisplay(props) {
         <div>
           <div className={classes.welcome}>Welcome!</div>
           <div className={classes.main}>
-            <div className={classes.info}>Main info</div>
+            <div className={classes.info}>
+              <h1 className={classes.textTitle}>JC-EarthWorks</h1>
+              <h4>We'd be happy to help with your next project.</h4>
+              <p className={classes.introText}>
+                Let us introduce ourselves, we're JC-EarthWorks. We are a two
+                man team comprising of John and Chris, who have been in the
+                business longer than we should care to share. We have experience
+                from building house from the ground up, all the way to
+                commericial construction working on projects worth hundreds of
+                millions. This include a variety of infrastructure projects with
+                various complexities, from cutting edge to everyday
+                construction.
+              </p>
+              <img
+                className={classes.mainPicture}
+                src="https://eastislandsrentals.com/wp-content/uploads/2019/02/cooper-river-bridge-charleston-bridge-run-ravenel-bridge.jpg"
+              />
+            </div>
           </div>
         </div>
       );
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,7 +3,7 @@ import nav from "./Navigation.module.css";
 function Navigation() {
   return (
     <header className={nav.navMain}>
-      <div className={nav.logo}>EarthWorks</div>
+      <div className={nav.logo}>JC-EarthWorks</div>
       <div className={nav.navButton}>Home</div>
       <div className={nav.navButton}>About us</div>
       <div className={nav.navButton}>Pricing/Estimates</div>
