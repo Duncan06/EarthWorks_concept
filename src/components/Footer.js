4680dbfe9c2c0cf12a6fb9db6051d47e31a8db97
Duncan06 <tylerdownie0@gmail.com>
2022-03-27 00:21:46 -0700

    Change footer text and position.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,6 @@ import classes from "./Footer.module.css";
 function Footer() {
   return (
     <footer className={classes.footerMain}>
-      <div className={classes.message}>We look forward to working with you.</div>
       <div className={classes.emailFooter}>j.c.earthworks@gmail.com</div>
     </footer>
   );
