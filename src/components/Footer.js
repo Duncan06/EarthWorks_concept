492864462f928ce00ed81c0f21735dcb3c34f89e
Duncan06 <tylerdownie0@gmail.com>
2022-03-26 23:49:32 -0700

    Add email to footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,8 @@ import classes from "./Footer.module.css";
 function Footer() {
   return (
     <footer className={classes.footerMain}>
-      <div>We look forward to working with you.</div>
+      <div className={classes.message}>We look forward to working with you.</div>
+      <div className={classes.emailFooter}>j.c.earthworks@gmail.com</div>
     </footer>
   );
 }
