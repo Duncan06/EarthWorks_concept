62ded9ac20903266185e939bf5ad88c64630050c
Duncan06 <tylerdownie0@gmail.com>
2022-03-30 11:39:34 -0700

    Add remove animations after first view.

--- src/App.js
+++ src/App.js
@@ -9,12 +9,16 @@ function App() {
   const [about, setAbout] = useState(false);
   const [price, setPrice] = useState(false);
   const [contact, setContact] = useState(false);
+  const [firstTimeAbout, setFirstTimeAbout] = useState(true);
+  const [firstTimePrice, setFirstTimePrice] = useState(true);
+  const [firstTimeContact, setFirstTimeContact] = useState(true);
 
   function nowSetHome() {
     setHome(true);
     setAbout(false);
     setPrice(false);
     setContact(false);
+    notFirstHome();
   }
 
   function nowSetAbout() {
@@ -22,6 +26,11 @@ function App() {
     setAbout(true);
     setPrice(false);
     setContact(false);
+    if (firstTimeAbout) {
+      setFirstTimeAbout(false);
+    } else {
+      notFirstAbout();
+    }
   }
 
   function nowSetPrice() {
@@ -29,6 +38,11 @@ function App() {
     setAbout(false);
     setPrice(true);
     setContact(false);
+    if (firstTimePrice) {
+      setFirstTimePrice(false);
+    } else {
+      notFirstPrice();
+    }
   }
 
   function nowSetContact() {
@@ -36,6 +50,36 @@ function App() {
     setAbout(false);
     setPrice(false);
     setContact(true);
+    if (firstTimeContact) {
+      setFirstTimeContact(false);
+    } else {
+      notFirstContact();
+    }
+  }
+
+  function notFirstHome() {
+    document.documentElement.style.setProperty("--animation-time-1", 0);
+    document.documentElement.style.setProperty("--animation-time-2", 0);
+    document.documentElement.style.setProperty("--animation-time-3", 0);
+    document.documentElement.style.setProperty("--animation-time-4", 0);
+  }
+
+  function notFirstAbout() {
+    document.documentElement.style.setProperty("--animation-time-5", 0);
+    document.documentElement.style.setProperty("--animation-time-6", 0);
+    document.documentElement.style.setProperty("--animation-time-7", 0);
+  }
+
+  function notFirstPrice() {
+    document.documentElement.style.setProperty("--animation-time-8", 0);
+    document.documentElement.style.setProperty("--animation-time-9", 0);
+    document.documentElement.style.setProperty("--animation-time-10", 0);
+    document.documentElement.style.setProperty("--animation-time-11", 0);
+  }
+
+  function notFirstContact() {
+    document.documentElement.style.setProperty("--animation-time-12", 0);
+    document.documentElement.style.setProperty("--animation-time-13", 0);
   }
 
   return (
@@ -46,7 +90,15 @@ function App() {
         changePrice={nowSetPrice}
         changeContact={nowSetContact}
       />
-      <MainDisplay home={home} about={about} price={price} contact={contact} />
+      <MainDisplay
+        home={home}
+        about={about}
+        price={price}
+        contact={contact}
+        firstAbout={firstTimeAbout}
+        firstPrice={firstTimePrice}
+        firstContact={firstTimeContact}
+      />
       <Footer />
     </div>
   );
