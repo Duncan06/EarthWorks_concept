d4c83e308780ebc006da601753bf012aee222455
Duncan06 <tylerdownie0@gmail.com>
2022-03-31 21:39:01 -0700

    Add svgs through material ui.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,29 @@
 import nav from "./Navigation.module.css";
+import HomeIcon from "@material-ui/icons/Home";
+import DescriptionIcon from "@material-ui/icons/Description";
+import PaymentsIcon from "@material-ui/icons/Payment";
+import WorkIcon from "@material-ui/icons/Work";
 
 function Navigation(props) {
   return (
     <header className={nav.navMain}>
       <div className={nav.logo}>J.C. EarthWorks</div>
-      <button className={nav.navButton} onClick={props.changeHome}>Home</button>
-      <button className={nav.navButton} onClick={props.changeAbout}>About us</button>
-      <button className={nav.navButton} onClick={props.changePrice}>Pricing/Estimates</button>
-      <button className={nav.navButton} onClick={props.changeContact}>Contact For Hire</button>
+      <button className={nav.navButton} onClick={props.changeHome}>
+        <HomeIcon />
+        Home
+      </button>
+      <button className={nav.navButton} onClick={props.changeAbout}>
+        <DescriptionIcon />
+        About us
+      </button>
+      <button className={nav.navButton} onClick={props.changePrice}>
+        <PaymentsIcon />
+        Pricing/Estimates
+      </button>
+      <button className={nav.navButton} onClick={props.changeContact}>
+        <WorkIcon />
+        Contact For Hire
+      </button>
     </header>
   );
 }
