cdc9b39a8782442477c5d605fb76b141a89a05a1
Duncan06 <tylerdownie0@gmail.com>
2022-04-03 14:38:25 -0700

    Refactor AboutPage and transfer @media to css.

--- src/components/MainDisplay.js
+++ src/components/MainDisplay.js
@@ -1,131 +1,13 @@
 import classes from "./MainDisplay.module.css";
-import WelcomePage from "./WelcomePage"
-
-
+import WelcomePage from "./WelcomePage";
+import AboutPage from "./AboutPage";
 
 function MainDisplay(props) {
   function CurrentInformation() {
     if (props.home) {
-      return (
-        <WelcomePage />
-      );
+      return <WelcomePage />;
     } else if (props.about) {
-      return (
-        <div>
-          <div className={classes.mainAboutContainer}>
-            <div className={classes.mainAbout}>
-              <h1 className={classes.innerTitle}>Experience</h1>
-              <p>
-                John and Chris have worked together professionally for over two
-                decades. They both have worked together under the professional
-                construction company FlatIron for years. In addition to this
-                They have taken on jobs as a two man team doing freelance work,
-                as well as personal work that would typically run for tens of
-                thousands of dollars if professionals were hired.
-              </p>
-              <p>
-                There most likely is not an aspect of construction that either
-                of them have not touched. Also their amount of hands on
-                experience with machinary is second to none. They have both
-                worked up and down the east coast for multiple years,
-                experienceing a range of different soils, materials, and
-                conditions to work with.
-              </p>
-              <p>
-                John and Chris have worked together professionally for over two
-                decades. They both have worked together under the professional
-                construction company FlatIron for years. In addition to this
-                They have taken on jobs as a two man team doing freelance work,
-                as well as personal work that would typically run for tens of
-                thousands of dollars if professionals were hired.
-              </p>
-              <p>
-                There most likely is not an aspect of construction that either
-                of them have not touched. Also their amount of hands on
-                experience with machinary is second to none. They have both
-                worked up and down the east coast for multiple years,
-                experienceing a range of different soils, materials, and
-                conditions to work with.
-              </p>
-              <p>
-                John and Chris have worked together professionally for over two
-                decades. They both have worked together under the professional
-                construction company FlatIron for years. In addition to this
-                They have taken on jobs as a two man team doing freelance work,
-                as well as personal work that would typically run for tens of
-                thousands of dollars if professionals were hired.
-              </p>
-              <p>
-                There most likely is not an aspect of construction that either
-                of them have not touched. Also their amount of hands on
-                experience with machinary is second to none. They have both
-                worked up and down the east coast for multiple years,
-                experienceing a range of different soils, materials, and
-                conditions to work with.
-              </p>
-              <p>
-                John and Chris have worked together professionally for over two
-                decades. They both have worked together under the professional
-                construction company FlatIron for years. In addition to this
-                They have taken on jobs as a two man team doing freelance work,
-                as well as personal work that would typically run for tens of
-                thousands of dollars if professionals were hired.
-              </p>
-              <p>
-                There most likely is not an aspect of construction that either
-                of them have not touched. Also their amount of hands on
-                experience with machinary is second to none. They have both
-                worked up and down the east coast for multiple years,
-                experienceing a range of different soils, materials, and
-                conditions to work with.
-              </p>
-            </div>
-            <div className={classes.mainAbout2}>
-              <h1 className={classes.innerTitle}>John's Background</h1>
-              <p>
-                John has worked professionally in construction for over 20
-                years. He has worked on numerous projects and has acquired the
-                following skillset:
-              </p>
-              <ul>
-                <li>Crane operation</li>
-                <li>Initial site setup</li>
-                <li>Electrical</li>
-                <li>Concrete</li>
-                <li>Carpentry</li>
-                <li>Soil Composite analysis</li>
-              </ul>
-              <img
-                className={classes.picAbout}
-                src="https://thumbs.dreamstime.com/b/lamborghini-aventador-lp-superveloce-test-drive-day-hong-kong-china-aug-aug-hong-kong-63226553.jpg"
-                alt="aboutPic1"
-              />
-            </div>
-            <div className={classes.mainAbout3}>
-              <h1 className={classes.innerTitle}>Chris's Background</h1>
-              <p>
-                Chris has worked in construction since before he was 18 years
-                old. He has worked for multiple construction companies as well
-                as taking on independent work when needed. Over the course of
-                his career he has gained the following skillset:
-              </p>
-              <ul>
-                <li>Crane operation</li>
-                <li>Initial site setup</li>
-                <li>Electrical</li>
-                <li>Concrete</li>
-                <li>Carpentry</li>
-                <li>Soil Composite analysis</li>
-              </ul>
-              <img
-                className={classes.picAbout}
-                src="https://thumbs.dreamstime.com/b/lamborghini-choun-buri-august-tron-supercars-aventador-car-display-thailand-super-series-race-august-bira-33090437.jpg"
-                alt="aboutPic2"
-              />
-            </div>
-          </div>
-        </div>
-      );
+      return <AboutPage />;
     } else if (props.price) {
       return (
         <div>
