082da64a5a75391b8ed69f1973c4d1b77ec3cb0a
Duncan06 <tylerdownie0@gmail.com>
2022-04-04 10:37:01 -0700

    Add animation, place buttons at bottom.

--- src/components/Option1Welcome.js
+++ src/components/Option1Welcome.js
@@ -12,15 +12,17 @@ function Option1(props) {
       <div className={classes.main}>
         <div className={classes.info}>
           <h1 className={classes.textTitle}>J.C. EarthWorks</h1>
-          <div className={classes.introHeader}>Foundation Leveling</div>
-          <p className={classes.introText}>
-            Getting a handle on foundation leveling.
-          </p>
-          <img
-            className={classes.mainPicture}
-            src="https://images.unsplash.com/photo-1599365726864-ac222011504a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGxhbmRzY2FwaW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-            alt="mainPic1"
-          />
+          <div className={classes.newSelection}>
+            <div className={classes.introHeader}>Foundation Leveling</div>
+            <p className={classes.introText}>
+              Getting a handle on foundation leveling.
+            </p>
+            <img
+              className={classes.mainPicture}
+              src="https://images.unsplash.com/photo-1599365726864-ac222011504a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGxhbmRzY2FwaW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+              alt="mainPic1"
+            />
+          </div>
           <div className={classes.buttonContainer}>
             <button className={classes.previousButton} onClick={props.previous}>
               Previous
--- src/components/Option4Welcome.js
+++ src/components/Option4Welcome.js
@@ -12,15 +12,17 @@ function Option4(props) {
       <div className={classes.main}>
         <div className={classes.info}>
           <h1 className={classes.textTitle}>J.C. EarthWorks</h1>
-          <div className={classes.introHeader}>Consultating</div>
-          <p className={classes.introText}>
-            Let us recommend some possibilities.
-          </p>
-          <img
-            className={classes.mainPicture}
-            src="https://images.unsplash.com/photo-1434626881859-194d67b2b86f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDd8fGJ1c2luZXNzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-            alt="mainPic1"
-          />
+          <div className={classes.newSelection4}>
+            <div className={classes.introHeader}>Consultating</div>
+            <p className={classes.introText}>
+              Let us recommend some possibilities.
+            </p>
+            <img
+              className={classes.mainPicture}
+              src="https://images.unsplash.com/photo-1434626881859-194d67b2b86f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDd8fGJ1c2luZXNzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+              alt="mainPic1"
+            />
+          </div>
           <div className={classes.buttonContainer}>
             <button className={classes.previousButton} onClick={props.previous}>
               Previous
