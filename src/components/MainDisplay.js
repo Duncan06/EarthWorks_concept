dc7c37f3a214ccd9725b1f96d67b9bf97b1d89f7
Duncan06 <tylerdownie0@gmail.com>
2022-04-09 19:27:48 -0700

    Add lazy load to MainDisplay.

--- src/components/MainDisplay.js
+++ src/components/MainDisplay.js
@@ -1,35 +1,51 @@
+import React, { Suspense } from "react";
 import classes from "./MainDisplay.module.css";
-import WelcomePage from "./pageOptions/WelcomePage";
-import AboutPage from "./pageOptions/AboutPage";
-import PricePage from "./pageOptions/PricePage";
-import ContactPage from "./pageOptions/ContactPage";
+
+const WelcomePage = React.lazy(() => import("./pageOptions/WelcomePage"));
+const AboutPage = React.lazy(() => import("./pageOptions/AboutPage"));
+const PricePage = React.lazy(() => import("./pageOptions/PricePage"));
+const ContactPage = React.lazy(() => import("./pageOptions/ContactPage"));
 
 function MainDisplay(props) {
   function CurrentInformation() {
     if (props.home) {
       return (
-        <WelcomePage
-          notHome={props.notHome}
-          option1={props.option1}
-          changeOption1={props.changeOption1}
-          option2={props.option2}
-          changeOption2={props.changeOption2}
-          option3={props.option3}
-          changeOption3={props.changeOption3}
-          option4={props.option4}
-          changeOption4={props.changeOption4}
-          option5={props.option5}
-          changeOption5={props.changeOption5}
-          next={props.next}
-          previous={props.previous}
-        />
+        <Suspense fallback={<div>Loading...</div>}>
+          <WelcomePage
+            notHome={props.notHome}
+            option1={props.option1}
+            changeOption1={props.changeOption1}
+            option2={props.option2}
+            changeOption2={props.changeOption2}
+            option3={props.option3}
+            changeOption3={props.changeOption3}
+            option4={props.option4}
+            changeOption4={props.changeOption4}
+            option5={props.option5}
+            changeOption5={props.changeOption5}
+            next={props.next}
+            previous={props.previous}
+          />
+        </Suspense>
       );
     } else if (props.about) {
-      return <AboutPage />;
+      return (
+        <Suspense fallback={<div>Loading...</div>}>
+          <AboutPage />
+        </Suspense>
+      );
     } else if (props.price) {
-      return <PricePage />;
+      return (
+        <Suspense fallback={<div>Loading...</div>}>
+          <PricePage />
+        </Suspense>
+      );
     } else {
-      return <ContactPage />;
+      return (
+        <Suspense fallback={<div>Loading...</div>}>
+          <ContactPage />
+        </Suspense>
+      );
     }
   }
   return <div className={classes}>{CurrentInformation()}</div>;
