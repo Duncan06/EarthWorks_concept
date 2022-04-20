eb1a4ec52c3671e385960fdf5bd2ea73c73a7761
Duncan06 <tylerdownie0@gmail.com>
2022-04-20 16:18:52 -0700

    Change format, initial error states, fix endDate.

--- src/components/PageOptions/CustomerValidation.js
+++ src/components/PageOptions/CustomerValidation.js
@@ -5,6 +5,11 @@ const initialState = {
   firstName: "",
   lastName: "",
   email: "",
+  firstNameError: "",
+  lastNameError: "",
+  emailError: "",
+  startDateError: "",
+  endDateError: "",
 };
 
 export default class CustomerValidation extends React.Component {
@@ -57,8 +62,8 @@ export default class CustomerValidation extends React.Component {
       startDateError = "Please provide an appropriate date.";
     }
 
-    if (!this.state.endDate || currentEndDate < date) {
-      endDateError = "Please provide an appropriate date.";
+    if (!this.state.endDate || currentEndDate < currentStartDate) {
+      endDateError = "Please provide a date after the start date.";
     }
 
     if (
@@ -93,98 +98,113 @@ export default class CustomerValidation extends React.Component {
   render() {
     return (
       <form className={classes.customerInfo} onSubmit={this.handleSubmit}>
-        <div className={classes.customerText}>
-          <label htmlFor="firstName">First Name: </label>
-          <input
-            id="firstName"
-            name="firstName"
-            placeholder="First Name"
-            value={this.state.firstName}
-            onChange={this.handleChange}
-          />
-          <div className={classes.textError}> {this.state.firstNameError} </div>
+        <div className={classes.topContainer}>
+          <div className={classes.textContainer}>
+            <div className={classes.customerText}>
+              <label htmlFor="firstName">First Name: </label>
+              <input
+                id="firstName"
+                name="firstName"
+                placeholder="First Name"
+                value={this.state.firstName}
+                onChange={this.handleChange}
+              />
+              <div className={classes.textError}>
+                {" "}
+                {this.state.firstNameError}{" "}
+              </div>
+            </div>
+            <div className={classes.customerText}>
+              <label htmlFor="lastName">Last Name: </label>
+              <input
+                id="lastName"
+                name="lastName"
+                placeholder="Last Name"
+                value={this.state.lastName}
+                onChange={this.handleChange}
+              />
+              <div className={classes.textError}>
+                {" "}
+                {this.state.lastNameError}{" "}
+              </div>
+            </div>
+            <div className={classes.customerText}>
+              <label htmlFor="email">Email: </label>
+              <input
+                id="email"
+                name="email"
+                placeholder="Example@gmail.com"
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
+              <div className={classes.textError}> {this.state.emailError} </div>
+            </div>
+          </div>
+
+          <div className={classes.checkboxContainer}>
+            <div className={classes.checkboxTitle}>Types of related work</div>
+            <div className={classes.jobOptions}>
+              <label htmlFor="foundationLeveling">Foundation Leveling</label>
+              <input
+                type="checkbox"
+                id="foundationLeveling"
+                name="foundationLeveling"
+                onChange={this.handleChange}
+              />
+              <label htmlFor="irrigation">Irrigation</label>
+              <input
+                type="checkbox"
+                id="irrigation"
+                name="irrigation"
+                onChange={this.handleChange}
+              />
+              <label htmlFor="siteAnalysis">Site Analysis</label>
+              <input
+                type="checkbox"
+                id="siteAnalysis"
+                name="siteAnalysis"
+                onChange={this.handleChange}
+              />
+              <label htmlFor="consulting">Consulting</label>
+              <input
+                type="checkbox"
+                id="consulting"
+                name="consulting"
+                onChange={this.handleChange}
+              />
+              <label htmlFor="codeCompliance">Code Compliance</label>
+              <input
+                type="checkbox"
+                id="codeCompliance"
+                name="codeCompliance"
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
         </div>
 
-        <div className={classes.customerText}>
-          <label htmlFor="lastName">Last Name: </label>
-          <input
-            id="lastName"
-            name="lastName"
-            placeholder="Last Name"
-            value={this.state.lastName}
-            onChange={this.handleChange}
-          />
-          <div className={classes.textError}> {this.state.lastNameError} </div>
-        </div>
-
-        <div className={classes.customerText}>
-          <label htmlFor="email">Email: </label>
-          <input
-            id="email"
-            name="email"
-            placeholder="Example@gmail.com"
-            value={this.state.email}
-            onChange={this.handleChange}
-          />
-          <div className={classes.textError}> {this.state.emailError} </div>
-        </div>
-
-        <div className={classes.checkboxTitle}>Types of related work</div>
-        <div className={classes.jobOptions}>
-          <label htmlFor="foundationLeveling">Foundation Leveling</label>
-          <input
-            type="checkbox"
-            id="foundationLeveling"
-            name="foundationLeveling"
-            onChange={this.handleChange}
-          />
-          <label htmlFor="irrigation">Irrigation</label>
-          <input
-            type="checkbox"
-            id="irrigation"
-            name="irrigation"
-            onChange={this.handleChange}
-          />
-          <label htmlFor="siteAnalysis">Site Analysis</label>
-          <input
-            type="checkbox"
-            id="siteAnalysis"
-            name="siteAnalysis"
-            onChange={this.handleChange}
-          />
-          <label htmlFor="consulting">Consulting</label>
-          <input
-            type="checkbox"
-            id="consulting"
-            name="consulting"
-            onChange={this.handleChange}
-          />
-          <label htmlFor="codeCompliance">Code Compliance</label>
-          <input
-            type="checkbox"
-            id="codeCompliance"
-            name="codeCompliance"
-            onChange={this.handleChange}
-          />
-        </div>
-
-        <div className={classes.dates}>
-          <label htmlFor="startDate">Desired start date:</label>
-          <input
-            type="date"
-            id="startDate"
-            name="startDate"
-            onChange={this.handleChange}
-          />
-          <div className={classes.textError}> {this.state.startDateError} </div>
-          <label htmlFor="endDate">Desired end date:</label>
-          <input
-            type="date"
-            id="endDate"
-            name="endDate"
-            onChange={this.handleChange}
-          />
-          <div className={classes.textError}> {this.state.endDateError} </div>
+        <div className={classes.dateContainer}>
+          <div className={classes.dates}>
+            <label htmlFor="startDate">Desired start date:</label>
+            <input
+              type="date"
+              id="startDate"
+              name="startDate"
+              onChange={this.handleChange}
+            />
+            <div className={classes.textError}>
+              {" "}
+              {this.state.startDateError}{" "}
+            </div>
+            <label htmlFor="endDate">Desired end date:</label>
+            <input
+              type="date"
+              id="endDate"
+              name="endDate"
+              onChange={this.handleChange}
+            />
+            <div className={classes.textError}> {this.state.endDateError} </div>
+          </div>
         </div>
 
         <button type="submit" className={classes.submitButton}>
